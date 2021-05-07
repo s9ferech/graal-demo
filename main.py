62c287cc1f0602b5e66b70f3a29920747c976c30
Felix Rech <s9ferech@gmail.com>
2021-05-07 17:55:03 +0100

    Add minimal demo code

---
+++ main.py
@@ -0,0 +1,11 @@
+import java
+
+queue = java.type("java.util.PriorityQueue")()
+queue.add(3)
+queue.add(1)
+queue.add(2)
+print(queue.poll())
+print(queue.poll())
+print(queue.poll())
+print(queue.poll())
+
